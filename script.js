1fcc1e3faa12bfd7737abcaefdb1136e09c9d8bf
Alexis Kuszaj <ALEXSALEH94@GMAIL.COM>
2023-06-30 23:55:52 -0500

    updates

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ skillItems.forEach((item) => {
     item.classList.toggle('highlight');
   });
 });
+
+function scrollToSection(aboutId) {
+  const about = document.querySelector(aboutId);
+  if (about) {
+    about.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+function scrollToSection(skillsId) {
+  const skills = document.querySelector(skillsId);
+  if (skills) {
+    skills.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+function scrollToSection(projectsId) {
+  const projects = document.querySelector(projectsId);
+  if (projects) {
+    projects.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+function scrollToSection(contactId) {
+  const contact = document.querySelector(contactId);
+  if (contact) {
+    contact.scrollIntoView({ behavior: 'smooth' });
+  }
+}
